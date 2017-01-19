9122dbeef22f254f2d4b7c9cb81e7ef5af722858
Florian <florian.ludot@gmail.com>
2017-01-20 00:10:36 +0100

    Error code

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>503 Service Unavailable.</title>
+        <title>500 Internal Server Error.</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">503 Service Unavailable.</div>
+                <div class="title">500 Internal Server Error.</div>
             </div>
         </div>
     </body>
